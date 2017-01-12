f1bf78671022a1c1fcfca7068cd39bc167baf82b
Matt <matt.leonard.22@gmail.com>
2017-01-12 15:40:48 -0800

    added action to form and app.get in express app

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
+app.post('/submission', function(req, res) {
+  res.send("You submitted a file!");
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
