8d7c3d3b22b21c0564ab171e06a806ce9c0ed3a0
Matt <matt.leonard.22@gmail.com>
2017-01-12 17:28:00 -0800

    removed app.use multer

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(multer());
 app.use('/', index);
 
 app.post('/submission', upload.single('doc'), function(req, res) {
